5024e8bcf7a3833ae8be6a305b55d4fd3c34f9e6
daavidsc-gh <david.dosmiltres@gmail.com>
2025-12-11 18:39:05 +0100

    Todo , menos nota

--- app/src/main/java/es/fpsumma/dam2/utilidades/ui/screens/tareas/ListadoAsignaturaScreen.kt
+++ app/src/main/java/es/fpsumma/dam2/utilidades/ui/screens/tareas/ListadoAsignaturaScreen.kt
@@ -3,6 +3,7 @@ package es.fpsumma.dam2.utilidades.ui.screens.tareas
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -14,10 +15,12 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilterChip
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -36,10 +39,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import es.fpsumma.dam2.utilidades.model.Asignatura
-import es.fpsumma.dam2.utilidades.model.Tarea
 import es.fpsumma.dam2.utilidades.ui.viewmodel.AsignaturasViewModel
-import es.fpsumma.dam2.utilidades.ui.viewmodel.TareasViewModel
-
 
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -80,8 +80,8 @@ fun ListadoAsignaturasScreen(navController: NavController, vm: AsignaturasViewMo
         Column(modifier = Modifier
             .fillMaxSize()
             .padding(innerPadding)
-            .padding(24.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp)) {
+            .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(4.dp)) {
 
             OutlinedTextField(
                 value = nombre,
@@ -90,7 +90,7 @@ fun ListadoAsignaturasScreen(navController: NavController, vm: AsignaturasViewMo
                 singleLine = true,
                 modifier = Modifier.fillMaxWidth()
             )
-            Spacer(Modifier.height(8.dp))
+            Spacer(Modifier.height(4.dp))
             OutlinedTextField(
                 value = tipoAsignatura,
                 onValueChange = { tipoAsignatura = it },
@@ -98,15 +98,24 @@ fun ListadoAsignaturasScreen(navController: NavController, vm: AsignaturasViewMo
                 singleLine = false,
                 modifier = Modifier.fillMaxWidth()
             )
-            Spacer(Modifier.height(8.dp))
-            OutlinedTextField(
-                value = trimestre,
-                onValueChange = { trimestre = it },
-                label = { Text("Trimestre") },
-                singleLine = false,
-                modifier = Modifier.fillMaxWidth()
-            )
-            Spacer(Modifier.height(8.dp))
+            Spacer(Modifier.height(4.dp))
+            Text(text = "Trimestre:", modifier = Modifier.padding(start = 4.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(4.dp)
+            ) {
+                listOf("1ª Eva", "2ª Eva", "3ª Eva").forEach { eva ->
+                    FilterChip(
+                        selected = (trimestre == eva),
+                        onClick = { trimestre = eva },
+                        label = { Text(eva) },
+                        leadingIcon = if (trimestre == eva) {
+                            { Icon(Icons.Default.Check, contentDescription = null) }
+                        } else null
+                    )
+                }
+            }
+            Spacer(Modifier.height(4.dp))
             OutlinedTextField(
                 value = nota.toString(),
                 onValueChange = { newValue ->
@@ -121,13 +130,13 @@ fun ListadoAsignaturasScreen(navController: NavController, vm: AsignaturasViewMo
                 singleLine = true,
                 modifier = androidx.compose.ui.Modifier.fillMaxWidth()
             )
-            Spacer(Modifier.height(8.dp))
+            Spacer(Modifier.height(4.dp))
             Button(
                 onClick = ::handleAddAsignatura,
                 modifier = Modifier.fillMaxWidth()
             ) { Text("Añadir nota") }
             HorizontalDivider(modifier.padding(vertical = 16.dp))
-/*
+
             LazyColumn(
                 modifier = modifier
                     .fillMaxSize()
@@ -137,18 +146,26 @@ fun ListadoAsignaturasScreen(navController: NavController, vm: AsignaturasViewMo
                 items(
                     items = asignatura,
                     key = { it.id }
-                ) { tarea ->
+                ) { asignatura ->
                     Card (
-                        modifier = modifier,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 8.dp),
                         shape = RoundedCornerShape(16.dp)
                     ) {
                         ListItem(
-                            headlineContent = { Text(tarea.titulo) },
-                            supportingContent = { Text(tarea.descripcion) },
+                            headlineContent = { Text(asignatura.nombre) },
+                            supportingContent = {
+                                Column {
+                                    Text(text = "Asignatura: ${asignatura.tipoAsignatura}")
+                                    Text(text = "Trimestre: ${asignatura.trimestre}")
+                                    Text(text = "Nota: ${asignatura.nota}")
+                                }
+                            },
                             trailingContent = {
                                 IconButton(
-                                    onClick = {handleDeleteAsignatura(asignatura)},
-                                    modifier = modifier.size(48.dp)
+                                    modifier = Modifier.size(48.dp),
+                                    onClick = { handleDeleteAsignatura(asignatura) }
                                 ) {
                                     Icon(
                                         imageVector = Icons.Outlined.Delete,
@@ -159,7 +176,7 @@ fun ListadoAsignaturasScreen(navController: NavController, vm: AsignaturasViewMo
                         )
                     }
                 }
-            }*/
+            }
         }
     }
 }
